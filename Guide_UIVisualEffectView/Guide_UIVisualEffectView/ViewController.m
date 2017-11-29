81418eead8b4a96deaad6544794f28bbda340490
肖伟 <xw@xiaoqian.com>
2017-11-29 17:36:23 +0800

    Add a blur view
    Add a blur view to app interface when it enter background.

---
+++ Guide_UIVisualEffectView/Guide_UIVisualEffectView/ViewController.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  Guide_UIVisualEffectView
+//
+//  Created by Mac on 2017/11/20.
+//  Copyright © 2017年 xiaowei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (nonatomic, strong) UIVisualEffectView *blurView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleAppResignActive) name:UIApplicationWillResignActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleAppBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];
+}
+
+- (void)handleAppResignActive
+{
+    [self.view addSubview:self.blurView];
+    self.blurView.frame = self.view.bounds;
+}
+
+- (void)handleAppBecomeActive
+{
+    [self.blurView removeFromSuperview];
+}
+
+- (UIVisualEffectView *)blurView
+{
+    if (!_blurView) {
+        UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleRegular];
+        _blurView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    }
+    return _blurView;
+}
+
+@end
---
+++ Guide_UIVisualEffectView/Guide_UIVisualEffectView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Guide_UIVisualEffectView
+//
+//  Created by Mac on 2017/11/20.
+//  Copyright © 2017年 xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
